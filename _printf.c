a3d75f8a6630cdb398956dea03cd80295fcc61f4
Vinceciana Kiboro <vinkwairimu17@gmail.com>
2023-08-23 03:54:16 -0700

    TASK 0 & 1

--- _printf.c
+++ _printf.c
@@ -4,15 +4,15 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
   *_printf - Printf function
-  *@formart: formart.
+  *@format: format.
   *Return: Printed chars.
   */
-int _printf(const char *formart, ...)
+int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff-ind = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	char buffer[BUFF SIZE];
+	char buffer [BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
